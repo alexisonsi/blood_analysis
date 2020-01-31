55231f1fe7fce2a867e99a2acde6ea2f63bc2d4f
Alexis Onsi <alexisonsi@yahoo.com>
2020-01-31 12:22:33 -0500

    Added test for borderline HDL and high LDL

--- test_chol_analysis.py
+++ test_chol_analysis.py
@@ -1,5 +1,17 @@
-def test_HDL_analysis():
+def test_HDL_analysis_normal():
     from chol_analysis import HDL_analysis 
     answer = HDL_analysis(80)
-    expected = "Normal"
+    expected = "normal"
+    assert answer == expected
+
+def test_HDL_analysis_bl():
+    from chol_analysis import HDL_analysis
+    answer = HDL_analysis(40)
+    expected = "borderline low"
+    assert answer == expected
+
+def test_LDL_analysis_high():
+    from chol_analysis import LDL_analysis
+    answer = LDL_analysis(165)
+    expected = "high"
     assert answer == expected
