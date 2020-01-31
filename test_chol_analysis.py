ddbf0dbf69128c56178f3cb0afa6b8a7f8c4bfd2
Alexis Onsi <alexisonsi@yahoo.com>
2020-01-31 12:43:55 -0500

    Fixed typos to get test to pass

--- test_chol_analysis.py
+++ test_chol_analysis.py
@@ -17,7 +17,7 @@ def test_LDL_analysis_high():
     assert answer == expected
 
 def test_fever_check():
-    from chol_analysis import test_fever_check
+    from chol_analysis import fever_check
     new_data = [96.0, 100.5, 105.1, 97]
     answer = fever_check(new_data)
     expected = True
